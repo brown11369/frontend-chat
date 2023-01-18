9f88f885d08cc1439c48ca871666cea795b373d8
brown <brown11369@gmail.com>
2023-01-18 17:37:05 +0530

    UI change and some bug fix

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host="http://localhost:8000";
+// export const host="http://localhost:8000";
+export const host="https://vito.onrender.com"
 export const registerRoute=`${host}/api/auth/register`;
 export const loginRoute=`${host}/api/auth/login`;
 export const setAvatarRoute=`${host}/api/auth/setavatar`;
